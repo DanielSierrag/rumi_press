419c2d6d1a1dc36c174bb17fa139723a6e3b77ba
danielsierrag <danielgreco@2209mail.com>
2025-05-02 10:52:25 -0500

    Deleting Author and Expense models

--- books/admin.py
+++ books/admin.py
@@ -9,16 +9,8 @@ class BookAdmin(admin.ModelAdmin):
     list_display = [
         field.name for field in models.Book._meta.fields if field.name not in ['id', 'authors']
     ]
-    search_fields = ('title', 'publisher', 'subtitle', 'authors__name')
+    search_fields = ('title', 'publisher', 'subtitle', 'authors')
     list_filter = ('category', 'published_date')
 
 
-@admin.register(models.Expense)
-class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ('value', 'category', 'book')
-    search_fields = ('category__name', 'book__title')
-    list_filter = ('book__category',)
-
-
 admin.site.register(models.Category)
-admin.site.register(models.Author)
--- books/models.py
+++ books/models.py
@@ -4,13 +4,6 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
@@ -35,11 +28,14 @@ class Expense(models.Model):
 
 class Book(models.Model):
     title = models.CharField(
-        max_length=200,
-        validators=[MinLengthValidator(3)]
+        max_length=400,
+        validators=[MinLengthValidator(2)]
     )
-    subtitle = models.CharField(max_length=200, blank=True, null=True)
-    authors = models.ManyToManyField(Author, related_name='books')
+    subtitle = models.CharField(max_length=500, blank=True, null=True)
+    authors = models.CharField(
+        max_length=500,
+        validators=[MinLengthValidator(2)]
+    )
     publisher = models.CharField(
         max_length=150,
         validators=[MinLengthValidator(3)]
@@ -49,8 +45,8 @@ class Book(models.Model):
         Category, on_delete=models.CASCADE,
         related_name='books', null=True, blank=True
     )
-    expense = models.OneToOneField(
-        Expense, on_delete=models.CASCADE, related_name='book'
+    expense = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True
     )
 
     def __str__(self):
